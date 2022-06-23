ad3fd45a078d9437b24b966351dc84d921c520ee
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-06-23 10:31:53 +0300

    use efffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Card from './Card';
-import { faker } from '@faker-js/faker';
-
+import styled from 'styled-components';
+const button = styled.button`
+background-color: #4CAF50; /* Green */
+border: none;
+color: white;
+padding: 15px 32px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;
+margin: 4px 2px;
+cursor: pointer;
+`
 function App() {
+ 
+  
 
 const [card, setCard] = useState([
   { 
@@ -22,6 +35,9 @@ const [card, setCard] = useState([
   title : 'developer',
   avatar : 'https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?size=338&ext=jpg&ga=GA1.2.1629513965.1654170963'},
   ])
+  useEffect(() => {
+    alert('use effect')
+  },[card])
 
   const [showCard,setshowCard] = useState(true)
   const toggleShowCard = () => setshowCard(!showCard)
